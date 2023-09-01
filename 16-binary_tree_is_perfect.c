9828ba64b935967a41adc8840dddfe0780ffc952
Enoch Taylor <tay121222@gmail.com>
2023-09-01 11:36:51 +0000

    function that checks if a binary tree is perfect xx

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,8 +17,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	l = binary_tree_level(tree->left);
 	r = binary_tree_level(tree->right);
 
-	if (l == r && binary_tree_is_full(tree))
-		return (1);
+	if (l == r)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
 
 	return (0);
 }
@@ -69,10 +75,10 @@ int binary_tree_level(const binary_tree_t *tree)
 {
 	int level = 0;
 
-	while (tree)
+	while (tree && tree->parent)
 	{
 		level++;
-		tree = tree->left;
+		tree = tree->parent;
 	}
 
 	return (level);
