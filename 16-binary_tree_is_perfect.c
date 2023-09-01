fbc842420be83caee027c1195cd4ecfddec74fb2
Enoch Taylor <tay121222@gmail.com>
2023-09-01 10:38:36 +0000

    function that checks if a binary tree is perfect x

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,7 +27,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  * binary_tree_is_full - function that checks if a binary tree is full
  * @tree: Pointer to the root node of the tree to check
  * Return: If tree is NULL, your function must return 0
- *  */
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
