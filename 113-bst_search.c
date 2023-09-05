1ec8c4217c53c7da07a8bd6676ef4bcbc4e6aa1d
Enoch Taylor <tay121222@gmail.com>
2023-09-05 13:32:29 +0000

    function that searches for a value in a Binary Search Tree

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -10,10 +10,10 @@
 bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (tree == NULL || tree->n == value)
-		return (bst_t *)tree;
+		return ((bst_t *)tree);
 
 	if (value < tree->n)
-		return bst_search(tree->left, value);
+		return (bst_search(tree->left, value));
 	else
-		return bst_search(tree->right, value);
+		return (bst_search(tree->right, value));
 }
