c283eabc1d626dd2bbc21529615303c199a08452
Enoch Taylor <tay121222@gmail.com>
2023-09-08 14:42:46 +0000

    function that inserts a value in an AVL Tree

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -8,7 +8,15 @@
  */
 int balance(const binary_tree_t *tree)
 {
-	return (tree != NULL ? (int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)) : 0);
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
 }
 
 /**
